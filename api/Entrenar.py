9283d50e5460feab2dd8327f0819fa525e5558bb
alezly <albanyramirez8@gmail.com>
2024-06-27 18:01:48 -0400

    wii

--- api/Entrenar.py
+++ api/Entrenar.py
@@ -8,36 +8,29 @@ from sklearn.metrics import r2_score
 data = pd.read_csv('dataset_tiempo.csv')
 
 # Seleccionar las características y la variable objetivo
-X = data[['distancia', 'diametro', 'altura', 'litro_x_minuto']]
-y = data['tiempo_llenado']
+X = data[["distance", "diametro","altura","volumen_tanque", "tiempo_tanque"]]
+y = data['tiempo']
 
 # Dividir el conjunto de datos en conjunto de entrenamiento y conjunto de prueba
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+print("Tamaño de Entrenamiento = {}".format(X_train.shape))
+print("Tamaño de Test          = {}".format(X_test.shape))
+
 # Inicializar el modelo de regresión lineal
 model = LinearRegression()
 
-# Entrenar el modelo
-model.fit(X_train.values, y_train.values)
-
-# Realizar predicciones
-predictions = model.predict(X_test.values)
+# Entrenar el model
+model.fit(X_train, y_train)
 
 # Cálculo de R^2 y R^2 ajustado en data de prueba
-r2_test = r2_score(y_test, model.predict(X_test))
+r2_test = r2_score(y_train, modelo.predict(X_train))
 
-n_test, n_vars = X_test.shape
-r2_test_ajustado = 1 - (1 - r2_test)*(n_test-1)/(n_test-n_vars-1)
+n_train, n_vars = X_train.shape
+
+r2_test_ajustado = 1 - (1 - r2_test)*(n_train-1)/(n_train-n_vars-1)
+r2_test_ajustado
 
 print("R2          en prueba = {:.5f}".format(r2_test))
 print("R2 ajustado en prueba = {:.5f}".format(r2_test_ajustado))
-
-
-# predicción ejemplo
-distancia = 50
-diametro = 100
-altura = 90
-litroxminuto = 5
-
-prediction = model.predict([[distancia, diametro, altura, litroxminuto]])
-print('Tiempo de llenado:', prediction)
+ 
